cd2c6b11d26cf07a21a9ba286b250f06b3fd43e8
potzko <orodnay@gmail.com>
2023-03-21 01:36:00 +0200

    added docs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+/*
+   small prime sieve I, will generate 100_000_000 in slightly under 10 seconds on my computer (using --release)
+   uses segmented sieve of eratosthenes to achive this.
+
+   I also made a small prime number struct so that you can just index into it without caring about implementation
+*/
+
 use core::cmp::min;
 use std::time;
 fn main() {
     let mut prime_nums = Primes::new();
     let a = time::Instant::now();
-    println!("{}", prime_nums.get(50_000_000));
+    println!("{}", prime_nums.get(100_000_000));
     println!("{:?}", a.elapsed());
 }
 
 #[derive(Debug)]
-struct Primes {
+pub struct Primes {
     prime_numbers: Vec<usize>,
     iterations: usize,
 }
@@ -16,6 +23,7 @@ struct Primes {
 impl Primes {
     fn new() -> Primes {
         Primes {
+            // we simulate having done the first iteration so that each segment will start with an odd number
             prime_numbers: vec![2, 3, 5, 7],
             iterations: 1,
         }
@@ -28,6 +36,7 @@ impl Primes {
     }
 }
 
+//this const was fast on my computer, you should mess around with it
 const MAX_JUMP: usize = 5;
 fn add_next_seg(primes: &mut Vec<usize>, iterations: &mut usize) {
     let seg_size = min(MAX_JUMP, primes.len() - *iterations - 1);
@@ -36,6 +45,11 @@ fn add_next_seg(primes: &mut Vec<usize>, iterations: &mut usize) {
     let seg_end = primes[end].pow(2);
     let seg_start = primes[start].pow(2);
     let mut primeness = vec![true; seg_end - seg_start];
+    /*
+       primeness is an array representing if the number in that relative index is prime
+       start and end are the last prime numbers for wheech we compleated the segment
+       seg_start, seg_end are the first and last numbers we will check in this iteration
+    */
     for i in 0..=end {
         let known_prime = primes[i];
         for not_prime in
